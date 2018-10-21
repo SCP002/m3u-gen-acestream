5f86ede874fc726c3be12ca4c96c7c91c94ddcaf
SCP002 <feedusfeedme@gmail.com>
2018-10-21 20:07:26 +0300

    Add getters to 'DataSet' class

--- src/config/data_set.py
+++ src/config/data_set.py
@@ -14,12 +14,48 @@ class DataSet:
                  filter_file_name: str,
                  filter_file_encoding: str,
                  clean_filter: bool) -> None:
-        self.json_url = json_url
-        self.resp_encoding = resp_encoding
-        self.out_file_name = out_file_name
-        self.out_file_encoding = out_file_encoding
-        self.out_file_first_line = out_file_first_line
-        self.out_file_format = out_file_format
-        self.filter_file_name = filter_file_name
-        self.filter_file_encoding = filter_file_encoding
-        self.clean_filter = clean_filter
+        self._json_url = json_url
+        self._resp_encoding = resp_encoding
+        self._out_file_name = out_file_name
+        self._out_file_encoding = out_file_encoding
+        self._out_file_first_line = out_file_first_line
+        self._out_file_format = out_file_format
+        self._filter_file_name = filter_file_name
+        self._filter_file_encoding = filter_file_encoding
+        self._clean_filter = clean_filter
+
+    @property
+    def json_url(self) -> str:
+        return self._json_url
+
+    @property
+    def resp_encoding(self) -> str:
+        return self._resp_encoding
+
+    @property
+    def out_file_name(self) -> str:
+        return self._out_file_name
+
+    @property
+    def out_file_encoding(self) -> str:
+        return self._out_file_encoding
+
+    @property
+    def out_file_first_line(self) -> str:
+        return self._out_file_first_line
+
+    @property
+    def out_file_format(self) -> str:
+        return self._out_file_format
+
+    @property
+    def filter_file_name(self) -> str:
+        return self._filter_file_name
+
+    @property
+    def filter_file_encoding(self) -> str:
+        return self._filter_file_encoding
+
+    @property
+    def clean_filter(self) -> bool:
+        return self._clean_filter
