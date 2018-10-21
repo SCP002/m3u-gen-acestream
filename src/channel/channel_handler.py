405e85b886eaa4ce3b9e2a7ed470a31decd8353c
SCP002 <feedusfeedme@gmail.com>
2018-10-21 23:13:37 +0300

    Partially extract functionality from M3UGenerator.main() to ChannelHandler.write_playlist()

--- src/channel/channel_handler.py
+++ src/channel/channel_handler.py
@@ -3,25 +3,64 @@
 
 
 from codecs import StreamReaderWriter
+from codecs import open
 from contextlib import closing
 from datetime import timedelta
 from json import loads
+from os import makedirs
+from os.path import dirname
 from sys import stderr
 from time import sleep
 from typing import List
 from urllib.error import URLError
 from urllib.request import urlopen
 
-from channel.channel import Channel, ChannelsDecoder
+from channel.channel import Channel
+from channel.channel import ChannelsDecoder
 from config.config import Config
 from config.data_set import DataSet
+from filter.filter_handler import FilterHandler
 from utils import Utils
 
 
 class ChannelHandler:
 
     @staticmethod
-    def fetch_channels(data_set: DataSet) -> List[Channel]:
+    def write_playlist(data_set: DataSet) -> None:
+        out_file_name: str = data_set.out_file_name
+        out_file_encoding: str = data_set.out_file_encoding
+        out_file_first_line: str = data_set.out_file_first_line
+
+        makedirs(dirname(out_file_name), exist_ok=True)
+
+        with closing(open(out_file_name, 'w', out_file_encoding)) as out_file:
+            out_file.write(out_file_first_line)
+
+            total_channel_count: int = 0
+            allowed_channel_count: int = 0
+
+            channels: List[Channel] = ChannelHandler._fetch_channels(data_set)
+            channels = FilterHandler.replace_categories(channels, data_set)
+            channels.sort(key=lambda x: x.name)
+            channels.sort(key=lambda x: x.category)
+
+            if data_set.clean_filter:
+                FilterHandler.clean_filter(channels, data_set)
+
+            for channel in channels:
+                total_channel_count += 1
+
+                if FilterHandler.is_channel_allowed(channel, data_set):
+                    ChannelHandler._write_entry(channel, data_set, out_file)
+                    allowed_channel_count += 1
+
+        print('Playlist', data_set.out_file_name, 'successfully generated.')
+        print('Channels processed in total:', total_channel_count)
+        print('Channels allowed:', allowed_channel_count)
+        print('Channels denied:', total_channel_count - allowed_channel_count)
+
+    @staticmethod
+    def _fetch_channels(data_set: DataSet) -> List[Channel]:
         json_url: str = data_set.json_url
         resp_encoding: str = data_set.resp_encoding
 
@@ -54,7 +93,7 @@ class ChannelHandler:
         return [Channel('', '', '')]
 
     @staticmethod
-    def write_entry(channel: Channel, data_set: DataSet, out_file: StreamReaderWriter) -> None:
+    def _write_entry(channel: Channel, data_set: DataSet, out_file: StreamReaderWriter) -> None:
         out_file_format: str = data_set.out_file_format
 
         entry: str = out_file_format \
--- src/m3u_generator.py
+++ src/m3u_generator.py
@@ -2,22 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
-from codecs import open
-from contextlib import closing
 from datetime import datetime
 from datetime import timedelta
-from os import makedirs
-from os.path import dirname
 from socket import gethostname, gethostbyname
 from sys import stderr
 from time import sleep
 from traceback import print_exc, format_exc
-from typing import List
 
-from channel.channel import Channel
 from channel.channel_handler import ChannelHandler
 from config.config import Config
-from filter.filter_handler import FilterHandler
 from utils import Utils
 
 
@@ -36,37 +29,7 @@ class M3UGenerator:
                 data_set_number += 1
                 print('Processing data set', data_set_number, 'of', len(Config.DATA_SETS))
 
-                out_file_name: str = data_set.out_file_name
-                out_file_encoding: str = data_set.out_file_encoding
-                out_file_first_line: str = data_set.out_file_first_line
-
-                makedirs(dirname(out_file_name), exist_ok=True)
-
-                with closing(open(out_file_name, 'w', out_file_encoding)) as out_file:
-                    out_file.write(out_file_first_line)
-
-                    total_channel_count: int = 0
-                    allowed_channel_count: int = 0
-
-                    channels: List[Channel] = ChannelHandler.fetch_channels(data_set)
-                    channels = FilterHandler.replace_categories(channels, data_set)
-                    channels.sort(key=lambda x: x.name)
-                    channels.sort(key=lambda x: x.category)
-
-                    if data_set.clean_filter:
-                        FilterHandler.clean_filter(channels, data_set)
-
-                    for channel in channels:
-                        total_channel_count += 1
-
-                        if FilterHandler.is_channel_allowed(channel, data_set):
-                            ChannelHandler.write_entry(channel, data_set, out_file)
-                            allowed_channel_count += 1
-
-                print('Playlist', data_set.out_file_name, 'successfully generated.')
-                print('Channels processed in total:', total_channel_count)
-                print('Channels allowed:', allowed_channel_count)
-                print('Channels denied:', total_channel_count - allowed_channel_count)
+                ChannelHandler.write_playlist(data_set)
 
                 if data_set_number < len(Config.DATA_SETS):
                     print('Sleeping for', timedelta(seconds=Config.DATA_SET_DELAY),
