8206998044e463b09f5eee7f5ee5098118963c5a
SCP002 <feedusfeedme@gmail.com>
2019-04-23 08:37:27 +0300

    Update TODO's

--- src/channel/channel_handler.py
+++ src/channel/channel_handler.py
@@ -93,10 +93,8 @@ class ChannelHandler:
         print('Channels denied:', total_channel_count - allowed_channel_count)
 
     # TODO: In case of death of the 'http://pomoyka.win/trash/ttv-list/'
-    #  fallback to the 'https://search.acestream.net'.
-
-    # TODO: In case if the 'https://search.acestream.net' becomes paid
-    #  fallback to the 'http://localhost:6878/server/api'.
+    #  fallback to the 'https://search.acestream.net/all?api_version=1.0&api_key=test_api_key'.
+    #  Skip if ('availability' < 0.8) or (now in seconds - 'availability_updated_at' > 8 * 86400).
     def _fetch_channels(self) -> List[Channel]:
         assert self.data_set is not None
 
