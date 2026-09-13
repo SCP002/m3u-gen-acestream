0835d8fc641d96e6866e957af45c367847157d34
SCP002 <22541678+SCP002@users.noreply.github.com>
2026-09-13 16:41:51 +0300

    Rewrite build.sh to iterate every available os/arch pair + avoid go env -w / go env -u

--- build.sh
+++ build.sh
@@ -1,49 +1,19 @@
 #!/bin/bash
 
 project_name="m3u_gen_acestream"
-main_file="${project_name}.go"
 build_path="build"
 
-os_list=(
-    "darwin"
-    "freebsd"
-    "linux"
-    "netbsd"
-    "openbsd"
-    "windows"
-)
+command -v go > /dev/null || { echo "go not found in PATH" >&2; exit 1; }
 
-arch_list=(
-    "386"
-    "amd64"
-    "arm"
-    "arm64"
-    "loong64"
-    "mips"
-    "mips64"
-    "mips64le"
-    "mipsle"
-    "s390"
-    "s390x"
-)
+mkdir -p "$build_path"
 
-for os in "${os_list[@]}"; do
-    if [[ $os == "windows" ]]; then
-        extension=".exe"
-    else
-        extension=""
-    fi
+for target in $(go tool dist list); do
+    os=${target%/*}
+    arch=${target#*/}
+    ext=""
+    [[ $os == "windows" ]] && ext=".exe"
+    [[ $arch == "wasm" ]] && ext=".wasm"
 
-    for arch in "${arch_list[@]}"; do
-        go env -w GOOS=$os 2> /dev/null
-        go env -w GOARCH=$arch 2> /dev/null
-
-        if [[ $? -eq 0 ]]; then
-            echo Building for $os / $arch
-            go build -o "${build_path}/${project_name}_${os}_${arch}${extension}" $main_file
-        fi
-    done
+    echo "Building for $os / $arch"
+    GOOS=$os GOARCH=$arch go build -o "${build_path}/${project_name}_${os}_${arch}${ext}" "${project_name}.go"
 done
-
-go env -u GOOS
-go env -u GOARCH
