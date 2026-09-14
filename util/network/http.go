91610526fe813003ae73508debfac3da6680d389
SCP002 <22541678+SCP002@users.noreply.github.com>
2026-09-14 08:28:32 +0300

    Docstring for network.NewHTTPClient: Mention is uses proxy from environment

--- util/network/http.go
+++ util/network/http.go
@@ -6,6 +6,7 @@ import (
 )
 
 // NewHTTPClient returns new HTTP client with a time limit for requests set to `timeout`.
+// The proxy is taken from the environment variables HTTP_PROXY, HTTPS_PROXY and NO_PROXY.
 func NewHTTPClient(timeout time.Duration) *http.Client {
 	return &http.Client{
 		Timeout: timeout,
