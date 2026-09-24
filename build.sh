b922cef2ef7247f7e470deb504bf5d579a929734
SCP002 <22541678+SCP002@users.noreply.github.com>
2026-09-24 07:18:26 +0300

    build.sh: Control CGO_ENABLED with top variable

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 project_name="m3u-gen-acestream"
 build_path="build"
 version="v3.0.0"
+cgo_enabled=0
 
 command -v go > /dev/null || { echo "go not found in PATH" >&2; exit 1; }
 
@@ -16,5 +17,5 @@ for target in $(go tool dist list); do
     [[ $arch == "wasm" ]] && ext=".wasm"
 
     echo "Building for $os / $arch"
-    CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -trimpath -ldflags "-X ${project_name}/version.Version=${version}" -o "${build_path}/${project_name}-${os}-${arch}${ext}" .
+    CGO_ENABLED=$cgo_enabled GOOS=$os GOARCH=$arch go build -trimpath -ldflags "-X ${project_name}/version.Version=${version}" -o "${build_path}/${project_name}-${os}-${arch}${ext}" .
 done
